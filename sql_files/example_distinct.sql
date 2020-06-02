86bfb641e8b87407bf50ed8b46dbcd283f609388
karenkathryn <karenwarmbein@gmail.com>
2020-06-01 18:50:05 -0700

    add example-keyword distinct

---
+++ sql_files/example_distinct.sql
@@ -0,0 +1,13 @@
+/*
+    Author:     Karen Warmbein 
+    Date:       June 1, 2020
+    Purpose:    Using keyword distinct to create a new view             
+    Note:       None
+*/
+
+
+SELECT COUNT(DISTINCT classification)  
+FROM solar_system_20;
+
+SELECT COUNT(classification) 
+FROM solar_system_20;
