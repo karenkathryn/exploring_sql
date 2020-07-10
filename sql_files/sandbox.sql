0e16ead6eec0901efb9d5f9fe7a83e2fc6f03c80
karenkathryn <karenwarmbein@gmail.com>
2020-07-09 18:00:17 -0700

    add lag and next function query to the codebase

--- sql_files/sandbox.sql
+++ sql_files/sandbox.sql
@@ -6,17 +6,26 @@
 */
 
 -- Finding duplicates from a table
-SELECT 
-    classification,
-    count(*)
-FROM solar_system_20
-GROUP BY classification
-HAVING count(*) > 2;
+-- SELECT 
+--     classification,
+--     count(*)
+-- FROM solar_system_20
+-- GROUP BY classification
+-- HAVING count(*) > 2;
 
 
 -- Using CONCAT and eliminating as for alising
-SELECT 
-    CONCAT_WS(' -- ', ss_name, classification) ss_info
+-- SELECT 
+--     CONCAT_WS(' -- ', ss_name, classification) ss_info
+-- FROM solar_system_20
+-- ORDER BY classification;
+
+
+SELECT ss_name
+    , diameter
+    , classification
+    , lag(ss_name, 1) over w as "previous"
+    , lead(ss_name, 1) over w as "next"
 FROM solar_system_20
-ORDER BY classification;
+window w as (ORDER BY diameter);
 
