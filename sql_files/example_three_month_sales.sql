8bb32ef9ab5abd4187ae98fc6581d91022e87241
karenkathryn <karenwarmbein@gmail.com>
2020-07-26 08:10:42 -0700

    update query  (three mothe sales) and add records to sales

--- sql_files/example_three_month_sales.sql
+++ sql_files/example_three_month_sales.sql
@@ -3,7 +3,7 @@
     Date:       July 23, 2020
     Purpose:    The purpose of this file is to solve an algorithmic problem            
     Note:       Write a query to display the records which have 3 or more 
-                consecutive rows and the amount of sales is more than 5000(inclusive)
+                consecutive rows and the amount of sales is equal to or more than mean
 */
 
 SELECT
@@ -14,9 +14,11 @@ FROM
     , net_sales
     , LAG(net_sales, 2) OVER(ORDER BY months) before_month
     , LAG(net_sales, 1) OVER(ORDER BY months) last_month
+    -- current month sales is net_sales
     , LEAD(net_sales, 1) OVER(ORDER BY months) second_month
     , LEAD(net_sales, 2) OVER(ORDER BY months) third_month
     FROM sales) s
-WHERE (s.net_sales>=5000 AND s.second_month>=5000 AND s.third_month>=5000) 
-      OR (s.before_month>=5000 AND s.last_month>=5000 AND s.net_sales>=5000) 
-      OR (s.last_month>=5000 AND s.second_month>=5000 AND s.net_sales>=5000)
+WHERE (s.net_sales>=7500 AND s.second_month>=7500 AND s.third_month>=7500) 
+      OR (s.before_month>=7500 AND s.last_month>=7500 AND s.net_sales>=7500) 
+      OR (s.last_month>=7500 AND s.net_sales>=7500 AND s.second_month>=7500)
+
