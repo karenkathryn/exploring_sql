af5e8d5c9456d363ddeb828b0a206df43e94a973
karenkathryn <karenwarmbein@gmail.com>
2020-08-15 19:04:53 -0700

    create tables and update file

--- sql_files/example_difficult_problem.sql
+++ sql_files/example_difficult_problem.sql
@@ -1,15 +1,13 @@
 SELECT 
-  Request_at AS 'Day'
-  , ROUND((COUNT(
-            CASE 
-                WHEN t.Status IN ('cancelled_due_to_weather') 
-                THEN 1 
-                ELSE NULL 
-            END
-            ) / COUNT(*) ), 2) AS `Cancellation Rate`
-FROM Trips t  
-JOIN Users d ON t.Driver_Id = d.Users_Id AND d.Banned = 'No'
-JOIN Users c ON t.Client_Id = c.Users_id AND c.Banned = 'No'
-WHERE Request_at BETWEEN '2013-10-01' AND '2013-10-03'
-GROUP BY Day
-ORDER BY Day;
+  oi.observation_date AS observing_date
+  , SUM(CASE 
+      WHEN oi.weather_status='cloudy' THEN 1 
+      ELSE 0  
+    END) AS cloudy
+  , COUNT(oi.observation_date) AS total
+  , (SUM(CASE 
+      WHEN oi.weather_status='cloudy' THEN 1 
+      ELSE 0  
+    END) / COUNT(oi.observation_date)) AS cancellation_rate
+FROM observation_info oi 
+GROUP BY observing_date;
---
+++ sql_files/files_for_table_creation/create_users_and_observations.sql
@@ -0,0 +1,56 @@
+/*
+    Author:     Karen Warmbein 
+    Date:       August 12, 2020
+    Purpose:    Create users and observations data tables                
+    Note:       None
+*/
+DROP TABLE IF EXISTS user_info;
+DROP TABLE IF EXISTS observation_info;
+
+CREATE TABLE user_info ( 
+    id INT,
+    sucessful TEXT, 
+    user_role TEXT);
+
+INSERT INTO user_info(id, sucessful, user_role)   
+VALUES (1, 'yes', 'astronomer'),
+    (2, 'no', 'astronomer'),
+    (3, 'no', 'astronomer'),
+    (4, 'yes', 'astronomer'),
+    (5, 'no', 'astronomer'),
+    (6, 'no', 'observatory'),
+    (7, 'yes', 'observatory'),
+    (8, 'no', 'observatory'),
+    (9, 'yes', 'observatory'),
+    (10, 'no', 'observatory'),
+    (11, 'no', 'observatory'),
+    (12, 'no', 'observatory'),
+    (13, 'yes', 'observatory'),
+    (14, 'yes', 'observatory'),
+    (15, 'no', 'observatory'),
+    (16, 'no', 'astronomer');
+
+CREATE TABLE observation_info ( 
+    id INT,
+    astronomer_id INT,
+    observatory_id INT,
+    location_id INT,
+    weather_status TEXT, 
+    observation_date TEXT);
+
+INSERT INTO observation_info(id, astronomer_id, observatory_id, location_id, weather_status, observation_date)   
+VALUES (1, 1, 6, 32, 'clear', '2016-05-03'),
+    (2, 2, 7, 31, 'clear', '2016-05-03'),
+    (3, 3, 8, 38, 'cloudy', '2016-05-03'),
+    (4, 4, 9, 40, 'clear', '2016-05-03'),
+    (5, 5, 10, 43, 'clear', '2017-06-30'),
+    (6, 6, 11, 42, 'cloudy', '2017-06-30'),
+    (7, 7, 12, 31, 'clear', '2017-06-30'),
+    (8, 8, 13, 43, 'clear', '2017-06-30'),
+    (9, 9, 14, 45, 'clear', '2017-06-30'),
+    (10, 10, 15, 35, 'clear', '2017-06-30'),
+    (11, 1, 11, 31, 'clear', '2017-06-30'),
+    (12, 5, 12, 43, 'cloudy', '2017-06-30'),
+    (13, 16, 10, 42, 'cloudy', '2018-05-01'),
+    (14, 3, 10, 39, 'clear', '2018-05-01'),
+    (15, 2, 10, 40, 'cloudy', '2018-05-01');
