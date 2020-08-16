55a00ad772d01345ea1e96f70a6105c31d159d97
karenkathryn <karenwarmbein@gmail.com>
2020-08-15 19:13:26 -0700

    update example  file to include rounding

--- sql_files/example_difficult_problem.sql
+++ sql_files/example_difficult_problem.sql
@@ -5,9 +5,12 @@ SELECT
       ELSE 0  
     END) AS cloudy
   , COUNT(oi.observation_date) AS total
-  , (SUM(CASE 
-      WHEN oi.weather_status='cloudy' THEN 1 
-      ELSE 0  
-    END) / COUNT(oi.observation_date)) AS cancellation_rate
+  , ROUND(
+      CAST(
+        SUM(CASE 
+          WHEN oi.weather_status='cloudy' THEN 1 
+          ELSE 0  
+        END) 
+      AS NUMERIC) / COUNT(oi.observation_date), 3) AS cancellation_rate
 FROM observation_info oi 
 GROUP BY observing_date;
