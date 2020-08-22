b0db78ee030b439a90c2e5e2a88966d83584c6dc
karenkathryn <karenwarmbein@gmail.com>
2020-08-22 15:28:27 -0700

    add more queries

--- sql_files/example_modifying_tables.sql
+++ sql_files/example_modifying_tables.sql
@@ -10,9 +10,17 @@ SELECT *
 FROM solar_system_20
 LIMIT 1;
 
--- Write a SQL statement to add a column 'distance' to the table locations.
+-- Write a SQL statement to add a column 'distance'.
 ALTER TABLE solar_system_20
 ADD distance INT;
+
+-- Insert data
+UPDATE solar_system_20 SET distance = 0 WHERE num = 1;
+UPDATE solar_system_20 SET distance = 478 WHERE num = 2;
+UPDATE solar_system_20 SET distance = 928 WHERE num = 3;
+UPDATE solar_system_20 SET distance = 1839 WHERE num = 4;
+UPDATE solar_system_20 SET distance = 2782 WHERE num = 5;
+
 
 -- Write a SQL statement to change the name of the column 'distance' to 
 -- 'distance_from_Sun', keeping the same size and data type and size.
@@ -27,3 +35,4 @@ ALTER distance_from_Sun TYPE FLOAT;
 -- Write a SQL statement to drop the column 'distance_from_Sun'.
 ALTER TABLE solar_system_20
 DROP distance_from_Sun;
+
