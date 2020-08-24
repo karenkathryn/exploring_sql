2d68db27ade9a161b36b3ad3e418881ecf8cf96c
karenkathryn <karenwarmbein@gmail.com>
2020-08-24 15:21:44 -0700

    finalize queries

--- sql_files/example_tuning.sql
+++ sql_files/example_tuning.sql
@@ -4,15 +4,20 @@
     Purpose:    The purpose of this file is to solve a problem, two ways 
                 and introduce tuning queries            
 */
-SELECT 
-  Score score
-  , DENSE_RANK() OVER (order by Score DESC) 'rank'
-FROM scores
+
 
 SELECT 
-    a.score
-    , COUNT(DISTINCT b.score) as 'rank'
-FROM scores a, scores b
-WHERE a.score <= b.score
-GROUP by a.id
-ORDER BY 2
+  id
+  , score
+  , DENSE_RANK() OVER (order by score DESC) "rank"
+FROM user_info
+WHERE user_role = 'observatory'
+ORDER BY id;
+
+SELECT 
+    t1.score
+    , COUNT(DISTINCT t2.score) as "rank"
+FROM user_info t1, user_info t2
+WHERE t1.score <= t2.score AND t1.user_role = 'observatory'
+GROUP by t1.id, t1.score
+ORDER BY t1.id;
