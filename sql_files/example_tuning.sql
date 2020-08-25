015867a52c960fc75992578b9e7197b8e0fb16cb
karenkathryn <karenwarmbein@gmail.com>
2020-08-25 13:49:33 -0700

    add output code to queries

--- sql_files/example_tuning.sql
+++ sql_files/example_tuning.sql
@@ -2,22 +2,32 @@
     Author:     Karen Warmbein 
     Date:       August 24, 2020
     Purpose:    The purpose of this file is to solve a problem, two ways 
-                and introduce tuning queries            
+                and introduce tuning queries      
+    Note:       Uses dense_rank function vs cross join  
+                Problem: Rank the scores.    
 */
 
-
+--  Planning Time: 0.147 ms
+--  Execution Time: 0.225 ms
+EXPLAIN ANALYZE
 SELECT 
   id
   , score
-  , DENSE_RANK() OVER (order by score DESC) "rank"
+  , DENSE_RANK() OVER (ORDER BY score DESC) "rank"
 FROM user_info
 WHERE user_role = 'observatory'
 ORDER BY id;
 
+
+--  Planning Time: 0.323 ms
+--  Execution Time: 0.818 ms
+EXPLAIN ANALYZE
 SELECT 
-    t1.score
+    t1.id
+    , t1.score
     , COUNT(DISTINCT t2.score) as "rank"
 FROM user_info t1, user_info t2
 WHERE t1.score <= t2.score AND t1.user_role = 'observatory'
 GROUP by t1.id, t1.score
 ORDER BY t1.id;
+
